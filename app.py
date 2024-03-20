27cfc87cdee35f71187834eacea19df99b7d9b13
Christopher Cuartas <ccuartas@uncc.edu>
2024-03-19 20:03:33 -0400

    Finsihed task 5, need to add tests

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 
 from src.repositories.movie_repository import get_movie_repository
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 
 # Get the movie repository singleton to use throughout the application
 movie_repository = get_movie_repository()
+
+movie_repository.create_movie("Life of Pi", "Ang Lee", 5)
+
+test = movie_repository.get_movie_by_title("Life of Pi").movie_id
+print(f"Testing: {test}")
 
 
 @app.get('/')
@@ -45,13 +50,22 @@ def get_single_movie(movie_id: int):
 
 @app.get('/movies/<int:movie_id>/edit')
 def get_edit_movies_page(movie_id: int):
-    return render_template('edit_movies_form.html')
+    movie = movie_repository.get_movie_by_id(movie_id)
+    return render_template('edit_movies_form.html', movie = movie)
 
 
 @app.post('/movies/<int:movie_id>')
 def update_movie(movie_id: int):
     # TODO: Feature 5
     # After updating the movie in the database, we redirect back to that single movie page
+    movie = movie_repository.get_movie_by_id(movie_id)
+    if request.form['title'] != '' :
+        movie.title =  request.form['title']
+    if request.form['director'] != '':
+        movie.director =  request.form['director']
+    if request.form['rating'] != '':
+        movie.rating =  request.form['rating']
+    print(f"{movie.director} {movie.title} {movie.rating}")
     return redirect(f'/movies/{movie_id}')
 
 
--- tests/unit/test_update_movie.py
+++ tests/unit/test_update_movie.py
@@ -1,1 +1,12 @@
 # TODO: Feature 5
+from src.models.movie import Movie
+
+
+def test_update_movie():
+    movie = Movie(123, 'Star Wars', 'George Lucas', 5)
+
+    assert type(movie) == Movie
+    assert movie.movie_id == 123
+    assert movie.title == 'Star Wars'
+    assert movie.director == 'George Lucas'
+    assert movie.rating == 5
