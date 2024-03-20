97abf1a369fb8ce125a6bb29be4110c963822f88
Matthew Wilkinson <mwilki31@uncc.edu>
2024-03-19 23:10:39 -0400

    Still working on tests committing for now

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 
 from src.repositories.movie_repository import get_movie_repository
 
@@ -33,9 +33,13 @@ def create_movie():
 
 @app.get('/movies/search')
 def search_movies():
-    # TODO: Feature 3
-    return render_template('search_movies.html', search_active=True)
-
+    # TODO: Feature 3   
+    title = request.args.get('title')
+    movie =movie_repository.get_movie_by_title(title)
+    if movie:
+        return render_template('search_movies.html', search_active = True, movie = movie)
+    else: 
+        return render_template('search_movies.html', search_active = True,error = "Movie Not Found") 
 
 @app.get('/movies/<int:movie_id>')
 def get_single_movie(movie_id: int):
--- tests/e2e/test_search_movies_page.py
+++ tests/e2e/test_search_movies_page.py
@@ -1,1 +1,43 @@
 # TODO: Feature 3
+
+from app import app 
+from src.repositories.movie_repository import get_movie_repository
+
+def test_search_movies():
+    test_app= app.test_client()
+
+    response = test_app.post('/movies',data = {
+        'Mname': 'Hunger Games',
+        'Dname': 'Gary Ross', 
+        'rating': '9'
+    }, follow_redirects = True)
+
+    assert response.status_code == 200
+
+
+    response = test_app.post('/movies',data = {
+        'Mname': 'The Matrix',
+        'Dname': 'Lana Wachowski', 
+        'rating': '9'
+    }, follow_redirects = True)
+
+    assert response.status_code == 200
+
+
+    search_response = test_app.get('/movies/search', query_string= {'title': 'Hunger Games'})
+    assert search_response.status_code ==200
+
+    assert b'Hunger Games' in search_response.data
+    assert b'Gary Ross' in search_response.data
+    assert b'9' in search_response.data
+
+
+    non_existing_search_response = test_app.get('/movies/search', query_string={'title': 'Diary of A Wimpy Kid'})
+    assert non_existing_search_response.status_code == 200
+
+    assert b'No Movie Found!' in non_existing_search_response.data
+
+    assert len(get_movie_repository().get_all_movies) == 2
+
+
+
