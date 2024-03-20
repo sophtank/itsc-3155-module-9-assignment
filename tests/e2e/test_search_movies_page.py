4fa765d5460b72b12649b640b4a0b1ac802c1793
Matthew Wilkinson <mwilki31@uncc.edu>
2024-03-20 13:10:21 -0400

    Updating tests

--- tests/e2e/test_search_movies_page.py
+++ tests/e2e/test_search_movies_page.py
@@ -1,43 +1,13 @@
 # TODO: Feature 3
-
-from app import app 
+from flask.testing import FlaskClient
 from src.repositories.movie_repository import get_movie_repository
 
-def test_search_movies():
-    test_app= app.test_client()
-
-    response = test_app.post('/movies',data = {
-        'Mname': 'Hunger Games',
-        'Dname': 'Gary Ross', 
-        'rating': '9'
-    }, follow_redirects = True)
-
-    assert response.status_code == 200
-
-
-    response = test_app.post('/movies',data = {
-        'Mname': 'The Matrix',
-        'Dname': 'Lana Wachowski', 
-        'rating': '9'
-    }, follow_redirects = True)
-
-    assert response.status_code == 200
-
-
-    search_response = test_app.get('/movies/search', query_string= {'title': 'Hunger Games'})
-    assert search_response.status_code ==200
-
-    assert b'Hunger Games' in search_response.data
-    assert b'Gary Ross' in search_response.data
-    assert b'9' in search_response.data
-
-
-    non_existing_search_response = test_app.get('/movies/search', query_string={'title': 'Diary of A Wimpy Kid'})
-    assert non_existing_search_response.status_code == 200
-
-    assert b'No Movie Found!' in non_existing_search_response.data
-
-    assert len(get_movie_repository().get_all_movies) == 2
-
+def test_search_movie_not_found(test_app: FlaskClient):
+    movie_repo =get_movie_repository()
+    movie_repo.clear_db()
+    response = test_app.get('/movies/search?title=Nonexistient Movie')
+    response_data = response.data.decode('utf-8')
+    assert response.status_code ==200
+    assert 'Movie Not Found' in response_data
 
 
--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -1,50 +1,20 @@
 # TODO: Feature 3
-from app import app
+from flask.testing import FlaskClient
 from src.repositories.movie_repository import get_movie_repository
 
-def test_search_movie_found():
-    test_app = app.test_client()
-    get_movie_repository().clear_db()
 
-
-    test_app.post('/movies', data = {
-        'Mname': 'Test Movie',
-        'Dname': 'Test Director',
-        'Rating' : 6
-
-    }, follow_redirects = True)
-
-
-    response = test_app.get('/movies/search', query_string = {'title':'Test Movie', 'rating': '4'})
-
-    assert response.status_code ==200
-    assert b'Test Movie' in response.data
-    assert b'Test Director' in response.data
-    assert b'4' in response.data
-
-def test_search_movie_not_found():
-    test_app = app.test_client()
-    get_movie_repository().clear_db()
-    response = test_app.get ('/movies/search', query_string = {'title' : 'Nonexistent Movie'})
-
-    assert response.status_code ==200
-
-
-def test_search_movie_empty_title():
-    test_app = app.test_client()
-    get_movie_repository().clear_db()
-
-
-    test_app.post ('/movies', data = {
-        'Mname': 'Test Movie',
-        'Dnmae': 'Test Director',
-        'rating': 9
-
-    }, follow_redirects = True)
-
-    response = test_app.get('/movies/search', query_string = {'title': " "})
+def test_search_movies():
+    movie_repo = get_movie_repository()
+    movie_repo.clear_db()
     
-    assert response.status_code==200
+    movie_repo.create_movie("Hunger Games", "Gary Ross", 4)
+    movie_repo.create_movie("The Matrix", "Lana Wachowski", 5)
 
+    search_result = movie_repo.get_movie_by_title("Hunger Games")
+    assert search_result is not None
+    assert search_result.title == "Hunger Games"
+    assert search_result.director == "Gary Ross"
+    assert search_result.rating == 4
 
-
+    non_existing_search_result = movie_repo.get_movie_by_title("Diary of A Wimpy Kid")
+    assert non_existing_search_result is None
