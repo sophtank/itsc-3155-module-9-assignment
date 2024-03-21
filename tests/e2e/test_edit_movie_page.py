f4eaf7b90976fbddd85856e611c539a76f670625
Christopher Cuartas <ccuartas@uncc.edu>
2024-03-20 23:42:53 -0400

    added edit button on view all movies page

--- tests/e2e/test_edit_movie_page.py
+++ tests/e2e/test_edit_movie_page.py
@@ -6,22 +6,20 @@ movie_repository = get_movie_repository()
 movie = movie_repository.create_movie("Life of Pi", "Ang Lee", 5)
 
 def test_edit_movie_page(test_app: FlaskClient):
-    id = movie_repository.get_movie_by_title("Life of Pi").movie_id
-
-    response = test_app.get(f"/movies/{id}/edit")
+    response = test_app.get(f"/movies/{movie.movie_id}/edit")
     response_data = response.data.decode('utf-8')
 
     assert response.status_code == 200
 
 
 def test_edited_movie_page(test_app: FlaskClient):
-    response = test_app.post(f"/movies/{movie.movie_id}", data ={
+    response2 = test_app.post(f"/movies/{movie.movie_id}", data ={
         "title" : "Star Wars",
         "director" : "George Lucas",
         "rating" : "4"
         }, follow_redirects=True)
     
-    assert response.status_code == 200
+    assert response2.status_code == 200
     
     assert movie.title == "Star Wars"
     assert movie.director == "George Lucas"
